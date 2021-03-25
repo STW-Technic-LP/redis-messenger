636869bcaa897e3336f6fef89159cd72aa527f7d
Nihat Özer <nihatforweb@gmail.com>
2021-03-25 12:48:21 +0300

    add single declaration file

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const redis = require('redis');
 const { Messenger } = require("./Messenger");
 
 module.exports = {
-   Messenger,
    /**
     * Creates and returns a messenger instance 
     * @param {number} port - defaults to 6379
