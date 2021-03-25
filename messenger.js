eb0603cbfc8a381cef97273c04d0df53749636cd
Nihat Özer <nihatforweb@gmail.com>
2021-03-25 13:25:47 +0300

    fix type

--- messenger.js
+++ messenger.js
@@ -190,7 +190,7 @@ class Messenger extends EventEmitter {
       });
    }
 }
-exports.Messenger = Messenger;
+module.exports = {Messenger};
 
 function makeId() {
    return new Date().getTime().toString(36) + Math.random().toString(36).substring(2);
